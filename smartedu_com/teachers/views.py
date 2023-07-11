d75f9dcd074c6560743c180c9af604b7b547f925
erdemaltu <e-altug@hotmail.com>
2023-07-11 20:02:38 +0300

    DetailView, Extra Content

--- smartedu_com/teachers/views.py
+++ smartedu_com/teachers/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from teachers.models import Teacher
+from courses.models import Course
 
 class TeacherListView(ListView):
     model = Teacher
@@ -11,4 +13,14 @@ class TeacherListView(ListView):
 
     # def get_queryset(self):
     #     return Teacher.objects.all()[:2]
+
+class TeacherDetailView(DetailView):
+    model = Teacher
+    template_name = 'teacher.html'
+    context_object_name = 'teacher'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['courses'] = Course.objects.filter(available=True, teacher= self.kwargs['pk'])
+        return context
     
