a2f05bade5dd4c0cf41ee7128274f1019e75f6e5
erdemaltu <e-altug@hotmail.com>
2023-07-11 15:16:31 +0300

    ListView,, Queryset, Paginate_by, Context Object

--- smartedu_com/courses/models.py
+++ smartedu_com/courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from teachers.models import Teacher
 
 class Category(models.Model):
     name = models.CharField(max_length=50, null=True)
@@ -17,6 +18,7 @@ class Tag(models.Model):
 
 
 class Course(models.Model):
+    teacher = models.ForeignKey(Teacher, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, unique=True)
     category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
     tags = models.ManyToManyField(Tag, blank=True, null=True)
