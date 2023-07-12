8492a3cdbd2a65e8e41d0be344564d50ec9950c9
erdemaltu <e-altug@hotmail.com>
2023-07-12 14:11:08 +0300

    Form View, Modal Form, Contact Page, CSRF

--- smartedu_com/pages/urls.py
+++ smartedu_com/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from pages.views import AboutView, IndexView
+from pages.views import AboutView, IndexView, ContactView
 
 urlpatterns = [
     # path(route, view,opt(kısayol ismi)),
     path('', IndexView.as_view(), name="index"),
     path('about/', AboutView.as_view(), name="about"),
+    path('contact/', ContactView.as_view(), name="contact"),
 ]
