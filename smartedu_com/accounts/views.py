1c4a277959bac5e7026fb3e990d0e97b45651221
erdemaltu <e-altug@hotmail.com>
2023-07-14 11:31:55 +0300

    Add, Modal Form, Course Registration

--- smartedu_com/accounts/views.py
+++ smartedu_com/accounts/views.py
@@ -3,6 +3,8 @@ from . forms import LoginForm, RegisterForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from courses.models import Course
+from django.contrib.auth.models import User
 
 def user_login(request):
     if request.method == 'POST':
@@ -44,8 +46,22 @@ def user_register(request):
 @login_required(login_url='login')
 def user_dashboard(request):
     current_user = request.user
-    return render(request, 'dashboard.html')
+    courses = current_user.courses_joined.all()
+
+    context = {
+        'courses': courses
+    }
+
+    return render(request, 'dashboard.html', context)
 
 def user_logout(request):
     logout(request)
     return redirect('index')
+
+def enroll_the_course(request):
+    course_id = request.POST['course_id']
+    user_id = request.POST['user_id']
+    course = Course.objects.get(id = course_id)
+    user = User.objects.get(id = user_id)
+    course.students.add(user)
+    return redirect('dashboard')
